1e7a5df0cd9de3884bd6e5f02dde6f9801a7df6c
SeungJun Yeom <johnyeom24@naver.com>
2021-12-06 15:56:42 +0900

    뭘까? segmentation fault

--- algorithm_p/basic_questions.cpp
+++ algorithm_p/basic_questions.cpp
@@ -120,3 +120,53 @@ int main(void)
 
 //1,000,000개이면 n log n으로 풀어야 한다. n^2으로 풀면 양이 어머어마함.
 
+//bj2751
+
+void quickSort(int* data, int start, int end)
+{
+	if (start >= end)
+		return;
+
+	int key = start;
+	int i = start + 1;
+	int j = end;
+	int temp;
+
+	while (i <= j)
+	{
+		while (data[i] <= data[key])
+			i++;
+		while (data[j] >= data[key] && j > start)
+			j--;
+		if (i > j)
+		{
+			temp = data[j];
+			data[j] = data[key];
+			data[key] = temp;
+		}
+		else
+		{
+			temp = data[j];
+			data[j] = data[i];
+			data[i] = temp;
+		}
+	}
+	quickSort(data, start, j - 1);
+	quickSort(data, j + 1, end);
+}
+
+int main(void)
+{
+	int size;
+	scanf("%d", &size);
+	int* arr = (int*)malloc(sizeof(int) * size);
+	for (int i = 0; i < size; i++)
+		scanf("%d", &arr[i]);
+
+	quickSort(arr, 0, size - 1);
+
+	for (int i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+
+	return 0;
+}
