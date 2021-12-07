c405260c900de3eaa4509a0e26ee4a3880c0aeed
SeungJun Yeom <johnyeom24@naver.com>
2021-12-07 22:36:25 +0900

    시간초과, 병합정렬

--- algorithm_p/basic_questions.cpp
+++ algorithm_p/basic_questions.cpp
@@ -122,7 +122,7 @@ int main(void)
 
 //bj2751
 
-void quickSort(int* data, int start, int end)
+/*void quickSort(int* data, int start, int end)
 {
 	if (start >= end)
 		return;
@@ -167,6 +167,47 @@ int main(void)
 
 	for (int i = 0; i < size; i++)
 		printf("%d ", arr[i]);
+
+	return 0;
+}*/
+
+int number, data[1000001];
+
+void quickSort(int* data, int start, int end) {
+	if (start >= end)
+		return;
+	int key = start;
+	int i = start + 1, j = end, temp;
+	while (i <= j) {
+		while (data[i] <= data[key]) {
+			i++;
+		}
+		while (data[j] >= data[key] && j > start) {
+			j--;
+		}
+		if (i > j) {
+			temp = data[j];
+			data[j] = data[key];
+			data[key] = temp;
+		}
+		else {
+			temp = data[i];
+			data[i] = data[j];
+			data[j] = temp;
+		}
+	}
+	quickSort(data, start, j - 1);
+	quickSort(data, j + 1, end);
+}
+
+int main(void) {
+	scanf("%d", &number);
+	for (int i = 0; i < number; i++) {
+		scanf("%d", &data[i]);
+	}
+	quickSort(data, 0, number - 1);
+	for (int i = 0; i < number; i++)
+		printf("%d\n", data[i]);
 
 	return 0;
 }
---
+++ algorithm_p/merge_sort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int number = 8;
+int sorted[8];  // 정렬 배열은 반드시 전역 변수로 선언.
+
+void merge(int a[], int m, int middle, int n) {
+	int i = m;
+	int j = middle + 1;
+	int k = m;
+	// 작은 순서대로 배열에 삽입.
+
+	while (i <= middle && j <= n) {
+		if (a[i] <= a[j]) {
+			sorted[k] = a[i];
+			i++;
+		}
+		else {
+			sorted[k] = a[j];
+			k++;
+		}
+		k++;
+	}
+	if (i > middle) {
+		for (int t = j; t <= n; t++) {
+			sorted[k] = a[t];
+			k++;
+		}
+	}
+	else {
+		for (int t = i; t <= middle; t++) {
+			sorted[k] = a[t];
+			k++;
+		}
+	}
+	// 정렬된 배열을 삽입.
+	for (int t = m; t <= n; t++) {
+		a[t] = sorted[t];
+	}
+}
+
+void mergeSort(int a[], int m, int n) {
+	if (m < n) {
+		int middle = (m + n) / 2;
+		mergeSort(a, m, middle);
+		mergeSort(a, middle + 1, n);
+		merge(a, m, middle,n);
+	}
+}
+
+int main(void)
+{
+	int arr[8] = { 7,6,5,8,3,5,9,1 };
+	mergeSort(arr, 0, number - 1);
+	for (int i = 0; i < number; i++)
+		printf("%d ", arr[i]);
+	return 0;
+}
